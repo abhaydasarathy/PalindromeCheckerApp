1b060b21a12ffa79b79cf1a8a49c0c65f85227a6
abhaydasarathy <abhaydasarathy2304@gmail.com>
2026-02-27 10:40:51 +0530

    Added Use Case 5 - Stack based palindrome checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
@@ -6,101 +7,38 @@ public class PalindromeCheckerApp {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("1. User Input Palindrome");
-        System.out.println("2. Hardcoded Palindrome");
-        System.out.println("3. Palindrome Using String Reverse (Loop)");
-        System.out.println("4. Palindrome Using Character Array (Two-Pointer)");
-        System.out.print("Enter choice: ");
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
 
-        int choice = sc.nextInt();
-        sc.nextLine(); // consume leftover newline
-
-        if (choice == 1) {
-
-            // UC1
-            System.out.print("Enter a word: ");
-            String word = sc.nextLine();
-            checkPalindrome(word);
-
-        } else if (choice == 2) {
-
-            // UC2
-            String hardcodedWord = "madam";
-            System.out.println("Hardcoded word: " + hardcodedWord);
-            checkPalindrome(hardcodedWord);
-
-        } else if (choice == 3) {
-
-            // UC3
-            System.out.print("Enter a word: ");
-            String word = sc.nextLine();
-            palindromeUsingReverse(word);
-
-        } else if (choice == 4) {
-
-            // UC4
-            System.out.print("Enter a word: ");
-            String word = sc.nextLine();
-            palindromeUsingCharArray(word);
-
-        } else {
-            System.out.println("Invalid choice");
-        }
+        checkPalindromeUsingStack(word);
 
         sc.close();
     }
 
-    // UC1 & UC2 - Basic Reverse Logic
-    public static void checkPalindrome(String word) {
+    // UC5 - Stack Based Palindrome Checker
+    public static void checkPalindromeUsingStack(String word) {
 
-        String reverse = "";
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reverse += word.charAt(i);
+        // Push characters into stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
-
-        if (word.equalsIgnoreCase(reverse))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
-    }
-
-    // UC3 - String Reverse Using Loop & Concatenation
-    public static void palindromeUsingReverse(String word) {
-
-        String reversed = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
-        }
-
-        if (word.equals(reversed))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
-    }
-
-    // UC4 - Character Array Two-Pointer Technique
-    public static void palindromeUsingCharArray(String word) {
-
-        char[] characters = word.toCharArray();
-
-        int start = 0;
-        int end = characters.length - 1;
 
         boolean isPalindrome = true;
 
-        while (start < end) {
+        // Pop and compare
+        for (int i = 0; i < word.length(); i++) {
 
-            if (characters[start] != characters[end]) {
+            char poppedChar = stack.pop();
+
+            if (word.charAt(i) != poppedChar) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
+        // Print result
         if (isPalindrome)
             System.out.println("Palindrome");
         else
