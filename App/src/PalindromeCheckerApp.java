f97d8e7b5299a7f8330018a4528bc58a42202420
abhaydasarathy <abhaydasarathy2304@gmail.com>
2026-02-27 10:24:05 +0530

    Added Use Case 2 - Hardcoded palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -6,32 +6,36 @@ public class PalindromeCheckerApp {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("1. Word Palindrome");
-        System.out.println("2. Number Palindrome");
+        System.out.println("1. Check User Input Palindrome");
+        System.out.println("2. Check Hardcoded Palindrome");
         System.out.print("Enter choice: ");
+
         int choice = sc.nextInt();
         sc.nextLine();
 
         if (choice == 1) {
+
+            // Use Case 1
             System.out.print("Enter a word: ");
             String word = sc.nextLine();
-            checkWordPalindrome(word);
-        }
+            checkPalindrome(word);
 
-        else if (choice == 2) {
-            System.out.print("Enter a number: ");
-            int num = sc.nextInt();
-            checkNumberPalindrome(num);
-        }
+        } else if (choice == 2) {
 
-        else {
+            // Use Case 2
+            String hardcodedWord = "madam";
+            System.out.println("Hardcoded word: " + hardcodedWord);
+            checkPalindrome(hardcodedWord);
+
+        } else {
             System.out.println("Invalid choice");
         }
 
         sc.close();
     }
 
-    public static void checkWordPalindrome(String word) {
+    public static void checkPalindrome(String word) {
+
         String reverse = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
@@ -39,21 +43,6 @@ public class PalindromeCheckerApp {
         }
 
         if (word.equalsIgnoreCase(reverse))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
-    }
-
-    public static void checkNumberPalindrome(int num) {
-        int original = num;
-        int reverse = 0;
-
-        while (num != 0) {
-            reverse = reverse * 10 + (num % 10);
-            num /= 10;
-        }
-
-        if (original == reverse)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
